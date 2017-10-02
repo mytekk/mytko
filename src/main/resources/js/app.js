e247f590d733415f7aed6169777f6d6f9a7eaf34
Bartłomiej Mytko <bartlomiej.mytko@gmail.com>
2017-10-02 20:41:15 +0200

    Started front-end: added tasks.html file; added app.js file with TaskCRUDService and TaskCRUDCtrl

---
+++ src/main/resources/js/app.js
@@ -0,0 +1,138 @@
+app.service('TaskCRUDService', ['$http', function ($http) {
+
+    this.getTask = function getTask(taskId) {
+        return $http({
+            method: 'GET',
+            url: 'tasks/' + taskId
+        });
+    }
+
+    this.addTask = function addTask(title, description) {
+        return $http({
+            method: 'POST',
+            url: 'tasks',
+            data: {
+                title: title,
+                description: description
+            }
+        });
+    }
+
+    this.updateTask = function updateTask(id, title, description) {
+        return $http({
+            method: 'PATCH',
+            url: 'tasks/' + id,
+            data: {
+                title: title,
+                description: description
+            }
+        });
+    }
+
+    this.deleteTask = function deleteTask(id) {
+        return $http({
+            method: 'DELETE',
+            url: 'tasks/' + id
+        })
+    }
+
+    this.getAllTasks = function getAllTasks() {
+        return $http({
+            method: 'GET',
+            url: 'tasks'
+        });
+    }
+
+}]);
+
+app.controller('TaskCRUDCtrl', ['$scope', 'TaskCRUDService',
+    function ($scope, TaskCRUDService) {
+        $scope.getTask = function () {
+            var id = $scope.task.id;
+            TaskCRUDService.getTask($scope.task.id)
+                .then(
+                    function success(response) {
+                        $scope.task = response.data;
+                        $scope.task.id = id;
+                        $scope.message = '';
+                        $scope.errorMessage = '';
+                    },
+                    function error(response) {
+                        $scope.message = '';
+                        if (response.status === 404) {
+                            $scope.errorMessage = 'Task not found!';
+                        }
+                        else {
+                            $scope.errorMessage = "Error getting task!";
+                        }
+                    }
+                );
+        };
+    },
+
+    $scope.addTask = function () {
+        if ($scope.task != null && $scope.task.title) {
+            TaskCRUDService.addTask($scope.task.title, $scope.task.description)
+                .then(
+                    function success(response) {
+                        $scope.message = 'Task added!';
+                        $scope.errorMessage = '';
+                    },
+                    function error(response) {
+                        $scope.message = '';
+                        $scope.errorMessage = 'Error adding task!';
+                    }
+                );
+        }
+        else {
+            $scope.message = '';
+            $scope.errorMessage = 'Please enter a title!';
+        }
+    },
+
+    $scope.updateTask = function () {
+        TaskCRUDService.updateTask($scope.task.id, $scope.task.title, $scope.task.description)
+            .then(
+                function success(response) {
+                    $scope.message = 'Task updated!';
+                    $scope.errorMessage = '';
+                },
+                function error(response) {
+                    $scope.message = '';
+                    $scope.errorMessage = 'Error updating task!';
+                }
+            );
+    },
+
+    $scope.deleteTask = function () {
+        TaskCRUDService.deleteTask($scope.task.id)
+            .then(
+                function success(response) {
+                    $scope.message = 'Task deleted!';
+                    $scope.errorMessage = '';
+                    $scope.task = null;
+                },
+                function error(response) {
+                    $scope.message = '';
+                    $scope.errorMessage = 'Error deleting task!';
+                }
+            )
+    },
+    
+    $scope.getAllTasks = function () {
+        TaskCRUDService.getAllTasks()
+            .then(
+                function success(response) {
+                    $scope.tasks = response.data._embedded.tasks;
+                    $scope.message = '';
+                    $scope.errorMessage = '';
+                },
+                function error(response) {
+                    $scope.tasks = null;
+                    $scope.message = '';
+                    $scope.errorMessage = "Error getting tasks!";
+                }
+            )
+    }
+
+]);
