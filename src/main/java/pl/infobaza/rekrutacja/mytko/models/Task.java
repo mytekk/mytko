0d7207fa62900ef033d22f28a28afcdfe3412082
Bartłomiej Mytko <bartlomiej.mytko@gmail.com>
2017-10-02 15:23:20 +0200

    Added credencials for H2 database; added constructor, getters and setters for Task entity; added method for search tasks by status in JpaStatusRepository

--- src/main/java/pl/infobaza/rekrutacja/mytko/models/Task.java
+++ src/main/java/pl/infobaza/rekrutacja/mytko/models/Task.java
@@ -31,4 +31,38 @@ public class Task {
     @Column
     private Boolean status;
 
+    public Task() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
 }
