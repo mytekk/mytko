e429aacd5d89a242c263691e862b509e256cb441
Bartłomiej Mytko <bartlomiej.mytko@gmail.com>
2017-10-02 13:09:07 +0200

    Initial commit

---
+++ src/main/java/pl/infobaza/rekrutacja/mytko/MytkoApplication.java
@@ -0,0 +1,12 @@
+package pl.infobaza.rekrutacja.mytko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytkoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytkoApplication.class, args);
+	}
+}
